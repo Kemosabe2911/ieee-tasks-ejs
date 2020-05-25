3c8c2a32df9ef6707161c001109caec9cec45bab
Kemosabe2911 <stevinprince29@gmail.com>
2020-05-25 16:58:44 +0530

    Add post

--- routes/tasks.js
+++ routes/tasks.js
@@ -7,4 +7,11 @@ router.get('/task1',(req,res) => res.render('task1'));
 //Task2
 router.get('/task2',(req,res) => res.render('task2'));
 
+//Task submission
+router.post('/task1',(req,res) =>{
+    console.log(req.body);
+    res.send('hello');
+})
+
+
 module.exports = router;
