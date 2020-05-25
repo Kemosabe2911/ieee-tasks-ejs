4907cc265084ac58162033eb8be5aac41424c5bd
Kemosabe2911 <stevinprince29@gmail.com>
2020-05-25 16:10:52 +0530

    change port

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.set('view engine','ejs');
 
 //Routes
 app.use('/',require('./routes/index'));
-app.use('/tasks',require('/routes/tasks');)
+app.use('/tasks',require('./routes/tasks'));
 
-const PORT= process.env.PORT || 3000;
+const PORT= process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running at ${PORT}`));
