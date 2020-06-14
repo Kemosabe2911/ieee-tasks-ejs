0e3edf8a2d7f6cbfa6a077a197e45ce98acec72c
Kemosabe2911 <stevinprince29@gmail.com>
2020-06-14 12:51:52 +0530

    Add tasks page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express= require('express');
 const router= express.Router();
 
+//Welcome Page
 router.get('/',(req,res) => res.render('welcome'));
+
+//Tasks Page
+router.get('/tasks',(req,res) => res.render('tasks'));
 
 module.exports = router;
